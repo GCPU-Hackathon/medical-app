49215a1545a7549cd9f29938eea049c0e46d8d95
Ilyas MOHETNA <ilyas.mohetna@hotmail.com>
2025-10-26 19:32:56 +0100

    attach medchatbot

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -65,7 +65,7 @@ class DashboardController extends Controller
      */
     private function checkAnalysisService(): array
     {
-        return HealthCheckService::checkServiceHealth('http://analysis:8000/health', 10);
+        return HealthCheckService::checkServiceHealth('http://medchatbot:8000/health', 10);
     }
 
     /**
